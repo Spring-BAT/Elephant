52338a6b1a7b3939456699b4616781741ada79d0
WMJ-Super1 <774167972@qq.com>
2019-11-03 23:28:10 +0800

    reading material

--- src/Samplework1/Fiction.java
+++ src/Samplework1/Fiction.java
@@ -3,9 +3,9 @@ package Samplework1;
 //小说类
 class Fiction extends Book {
     public Fiction(int pages, double price) {
-        this.pages = pages;
-        this.price = price;
-    }
+    this.pages = pages;
+    this.price = price;
+}
     public void Info() {
         System.out.println("This is a fiction,which has" +pages+ "pages,"+"costs"+price+"yuan.");
     }
